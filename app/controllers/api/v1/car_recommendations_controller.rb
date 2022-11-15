1ce260c4cef80a56c1d101140cef2d9bfb9663f5
Artem Belankov <temanmd@gmail.com>
2022-11-15 10:42:52 +0600

    Use CarRecommendationService in controller, fix test

--- app/controllers/api/v1/car_recommendations_controller.rb
+++ app/controllers/api/v1/car_recommendations_controller.rb
@@ -2,7 +2,14 @@ module API
   module V1
     class CarRecommendationsController < ApplicationController
       def index
-        render json: []
+        result = ::CarRecommendationsService.perform(actual_params)
+        render json: { data: result.data, errors: result.errors }
+      end
+
+      private
+
+      def actual_params
+        params.permit(:user_id, :page)
       end
     end
   end
--- test/controllers/api/v1/car_recommendations_controller_test.rb
+++ test/controllers/api/v1/car_recommendations_controller_test.rb
@@ -2,12 +2,15 @@ require 'test_helper'
 
 class API::V1::CarRecommendationsControllerTest < ActionDispatch::IntegrationTest
   test 'get a list of cars' do     
-    get api_v1_car_recommendations_path
+    get api_v1_car_recommendations_path(user_id: 1, page: 1)
 
     assert_response :success  
     
-    cars = JSON.parse(response.body)
-      
+    response = JSON.parse(response.body)
+    cars = response[:cars]
+    errors = response[:errors]
+
+    assert_empty errors
     assert_equal cars.length, 3
     assert_equal 'Volkswagen', cars.first.dig(:brand, :name)
   end
