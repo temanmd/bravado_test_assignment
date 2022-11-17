30d4b611abca7f0ca523accaf0b24887be99d04d
Artem Belankov <temanmd@gmail.com>
2022-11-17 11:34:44 +0600

    Sort cars in-memory -> SQL

--- app/services/car_recommendations_service.rb
+++ app/services/car_recommendations_service.rb
@@ -12,12 +12,10 @@ class CarRecommendationsService
 
   def perform
     @user = User.find(@params[:user_id])
-    @user_preferred_brands = @user.preferred_brands.load
 
     cars = get_all_recommended_cars()
     cars = filter_cars(cars)
-    cars = collect_cars_fields(cars)
-    cars = sort_cars(cars)
+    cars = sort_cars_sql(cars)
     cars = paginate_cars(cars)
 
     { errors: [], data: cars }
@@ -26,14 +24,17 @@ class CarRecommendationsService
   private
 
   def get_all_recommended_cars
-    recommended_cars = Car.includes(:user_car_recommendations)
-                           .select('cars.*', :rank_score)
+    recommended_cars = Car.includes(:brand, user_car_recommendations: [user: :preferred_brands])
+                           .select(
+                             'cars.*', :rank_score,
+                             select_label_sql
+                           )
                            .where(
                              user_car_recommendations: {
                                user_id: @params[:user_id]
                              }
                            )
-    recommended_cars.or(Car.all).includes(:brand)
+    recommended_cars.or(Car.all)
   end
 
   def filter_cars(cars)
@@ -72,6 +73,33 @@ class CarRecommendationsService
         label: car[:label]
       }
     end
+  end
+
+  def preferred_price_borders
+    from = @user.preferred_price_range.first
+    to = @user.preferred_price_range.last
+
+    [from, to].join(',')
+  end
+
+  def preferred_brand_ids_sql
+    UserPreferredBrand.where(user_id: 1).select(:brand_id).to_sql
+  end
+
+  def select_label_sql
+    match_preferred_brands_sql = "brands.id = ANY (#{preferred_brand_ids_sql})"
+    match_preffered_price_sql = "int8range(#{preferred_price_borders}) @> cars.price::int8"
+
+    "CASE WHEN #{match_preferred_brands_sql} AND "\
+              "#{match_preffered_price_sql} "\
+              "THEN 0 "\
+         "WHEN #{match_preferred_brands_sql} THEN 1 "\
+         "ELSE 2 "\
+    "END AS label"
+  end
+
+  def sort_cars_sql(cars)
+    cars.order('8, rank_score DESC NULLS LAST, cars.price')
   end
 
   def sort_cars(cars)
